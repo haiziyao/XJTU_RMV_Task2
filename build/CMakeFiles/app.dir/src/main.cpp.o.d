a8a8f10c6c3d67a9d4a7451b0dd4dc5f565893ae
haiziyao <1754203003@qq.com>
2025-09-20 17:28:57 +0800

    Delete build directory

--- build/CMakeFiles/app.dir/src/main.cpp.o.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/app.dir/src/main.cpp.o: \
- /home/hzy/RMV/opencv_project/src/main.cpp /usr/include/stdc-predef.h \
- /home/hzy/RMV/opencv_project/src/learn.h \
- /home/hzy/RMV/opencv_project/src/work.h \
- /home/hzy/RMV/opencv_project/src/findhsv.h \
- /home/hzy/RMV/opencv_project/src/HSVfind.h
